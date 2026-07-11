4f15c508300f4d27f10e45af737b65752e175569
thonghp <thonghoangpham123@gmail.com>
2026-07-12 01:44:05 +0700

    update y nghia cua ten

--- js/data/contentName.js
+++ js/data/contentName.js
@@ -1,32 +1,32 @@
 // File: js/contentName.js
 
 const soulContent = {
-  1: "Nhu cầu diễn đạt tự do theo phương thức cá nhân lựa chọn, khát khao tự do mạnh, cần thời gian cho riêng mình.",
+  1: "Khao khát sự tự do biểu đạt cá nhân, cần thời gian riêng tư để thư giãn hoặc làm nghệ thuật.",
   2: "Thiên hướng về sự cân bằng, toả ra cảm giác hài hoà, trực giác tốt, thích những gì thuộc về tự nhiên, trong giao tiếp thì công bằng và mong đợi sự công bằng.",
   3: "Khả năng đánh giá con người và tình huống khá tốt, lợi thế cho hoạt động kinh doanh và chuyên nghiệp.",
-  4: "Có ý kiến quan điểm chuẩn mực và truyền thống trong các lĩnh vực cuộc sống (tôn giáo, tình yêu, hôn nhân), giữ cảm xúc cân bằng.",
-  5: "Có tầng cảm xúc và nhu cầu tự do thể hiện cảm xúc (trừ trường hợp có mũi tên trống 4-5-6 thì dễ nhạy cảm và tổn thương).",
-  6: "Luôn hướng về sáng tạo và tình yêu thương, giữ vững tinh thần nếu không sức sáng tạo sẽ chuyển thành cảm giác tuyệt vọng, trăn trở khổ não.",
+  4: "Có ý kiến quan điểm chuẩn mực truyền thống, bảo thủ trong các lĩnh vực cuộc sống (tôn giáo, tình yêu, hôn nhân), giữ cảm xúc cân bằng và yêu trật tự.",
+  5: "Cảm xúc cực kỳ sâu sắc, khao khát mãnh liệt sự tự do để bộc lộ cảm xúc đó, trường hợp có mũi tên trống 456 thì dễ nhạy cảm và tổn thương.",
+  6: "Luôn hướng về sáng tạo và tình yêu thương (đặc biệt với gia đình), giữ vững tinh thần nếu ko sức sáng tạo sẽ chuyển thành cảm giác tuyệt vọng, trăn trở lo âu.",
   7: "Tự trải nghiệm đời thông qua các bài học kiểu riêng mình và truyền đạt lại cho người khác, phải trả giá đắt cho các bài học này cho đến khi nhận thức được mối quan hệ hai chiều được dạy và dạy.",
-  8: "Yêu thích độc lập và cương quyết từ chối những điều họ thấy vô lý, hành xử và suy nghĩ tự do độc lập, đồng thời nên cẩn thận lưu ý để không tách biệt khỏi cộng đồng.",
-  9: "Theo 2 khuynh hướng, tích cực thì cải thiện chất lượng cuộc sống con người, ý thức cộng đồng rõ nét. Tiêu cực thì dễ tham vọng thái quá, thiên về cái tôi, thường không thành công.",
+  8: "Yêu thích độc lập, hành xử và suy nghĩ tự do độc lập và cương quyết từ chối những điều họ thấy vô lý, lưu ý tránh trở nên quá xa cách, lạnh lùng để không tách biệt khỏi cộng đồng.",
+  9: "Theo 2 khuynh hướng, tích cực thì khao khát cải thiện chất lượng cuộc sống con người, ý thức cộng đồng rõ nét, còn tiêu cực thì dễ tham vọng thái quá, thiên về cái tôi, thường ko thành công.",
   10: "Độ linh hoạt về các vấn đề siêu hình/tâm linh, sức mạnh tâm hồn phong phú, có khả năng vô hiệu hoá những giới hạn xã hội đặt ra hoặc giới hạn hiểu biết của thế giới hiện tại nếu sử dụng khôn ngoan và đúng mực vốn tâm linh này từ đó hướng tới cảm nhận sâu và thanh khiết hơn về bản chất cuộc sống.",
-  11: "Giá trị tâm linh đặc biệt, sức mạnh trực giác quý giá, lòng thương và tâm từ nâng cao, đồng điệu và thấu cảm với người khác."
+  11: "Giá trị tâm linh đặc biệt, sức mạnh trực giác nhạy bén, lòng thương và tâm từ nâng cao, đồng điệu và thấu cảm với người khác.",
 };
 
 const expressContent = {
   1: "Thích thể thao 1 mình hoặc làm việc 1 mình (tự tin độc lập), cần sự tự do để làm việc theo nhịp độ bản thân.",
   2: "Thích làm việc nhóm với môi trường vui vẻ, là người thích điều vui giải trí hài hước nhẹ nhàng, khả năng thưởng thức được những hoạt động có tổ chức.",
   3: "Thích đem đến niềm vui, tiếng cười cho mọi người, là linh hồn của những buổi tiệc, thông minh nhanh nhẹn sáng sủa.",
-  4: "Thực tế, khéo tay, yêu thích thể thao hay các hoạt động xây dựng, sửa chữa.",
-  5: "Nhu cầu tự do thể hiện thoát khỏi ràng buộc giới hạn của thể chất, nên tìm những dạng công việc không gò bó để phát triển hết năng lực bản thân.",
+  4: "Thực tế, khéo tay, yêu thích thể thao hay các hoạt động xây dựng, sửa chửa.",
+  5: "Nhu cầu tự do thể hiện thoát khỏi ràng buộc giới hạn của thể chất vật chất, nên tìm những dạng công việc không gò bó để phát triển hết năng lực bản thân, ko e dè để tránh kìm nén.",
   6: "Tập trung nhiều năng lượng và tâm trí về gia đình, lưu ý cẩn trọng chiều chuộng quá mức, cân bằng giữa tình thương và nhu cầu thực sự giữa các thành viên.",
   7: "Hướng về tự làm việc mình, tự thân vận động và học theo cách riêng của bản thân, thích cảm giác thành tựu riêng khi chính bản thân đạt được hơn là học từ người khác.",
   8: "Khao khát độc lập thích làm 1 mình, dám tỏ ra khác biệt khi cần, bộc lộ tính cách mạnh mẽ.",
-  9: "Thể hiện 2 khía cạnh cuộc sống: nghiêm khắc và hài hước. Chỉ số 9 thường nhấn mạnh về nghiêm túc, dễ hút về nỗi buồn và cô độc, khả năng suy ngẫm và phân tích sâu bằng lý tưởng cao độ sẵn có, chuẩn mực sống tốt đẹp, cần cân bằng cuộc sống để tạo thêm niềm vui nhẹ nhàng, tái tạo sinh lực.",
-  10: "Nhấn nhiều về tính hài hước và có phần xem nhẹ tính nghiêm túc, cần tránh sự đùa giỡn không đúng chỗ, khả năng thích nghi với mọi hoàn cảnh cuộc sống, cần thể hiện quyết tâm mạnh hơn để hoàn tất nhiệm vụ trong cuộc sống này, cân bằng nghiêm túc với nhẹ nhàng.",
-  11: "Sự hài hoà cân bằng giữa những biểu đạt và cảm xúc và tinh thần để kiểm soát cảm xúc và hướng tới hạnh phúc an lạc.",
-  22: "Sức mạnh lớn trong tổ chức kinh doanh hoặc hoạt động thương mại. Lưu ý duy trì trạng thái cân bằng vì dễ lệch vào tập trung kiếm tiền. Gắn bó cống hiến với các tổ chức mang đến phúc lợi cho trẻ em khó khăn, từ đó đạt đến sự cân bằng trong cuộc sống."
+  9: "thể hiện 2 khía cạnh cuộc sống, nghiêm khắc và hài hước, thường nhấn mạnh về nghiêm túc, dễ hút về nỗi buồn và cô độc, khả năng suy ngẫm và phân tích sâu bằng lý tưởng cao độ sẵn có, chuẩn mực sống tốt đẹp, cần cân bằng cuộc sống để tạo thêm niềm vui nhẹ nhàng, tái tạo sinh lực.",
+  10: "Khả năng thích nghi với mọi hoàn cảnh cuộc sống, nhấn nhiều về tính hài hước và có phần xem nhẹ tính nghiêm túc (hời hợt), cần tránh sự đùa giỡn không đúng chỗ, cần thể hiện quyết tâm mạnh hơn để hoàn tất nhiệm vụ trong cuộc sống này, cân bằng nghiêm túc với nhẹ nhàng.",
+  11: "Khát khao biểu hiện sự hài hòa, mong muốn cân bằng cảm xúc tinh thần và thấu hiểu cuộc sống.",
+  22: "Sức mạnh lớn trong tổ chức kinh doanh hoặc hoạt động thương mại, (nếu có số chủ đạo là 4 8 22) thì lưu ý duy trì trạng thái cân bằng vì dễ lệch vào tập trung kiếm tiền, gắn bố cống hiến với các tổ chức mang đến phúc lợi cho trẻ em khó khăn, từ đó đạt đến sự cân bằng trong cuộc sống.",
 };
 
 export { soulContent, expressContent };
