088d705aa5e9348054ed8cd234068cae27ca6d01
thonghp <thonghoangpham123@gmail.com>
2026-07-12 11:28:02 +0700

    update thap dinh cao

--- js/data/contentPeak.js
+++ js/data/contentPeak.js
@@ -1,15 +1,15 @@
 // File: js/contentPeaks.js
 
 const peakNumbersContent = {
-  1: "Có những thay đổi lớn về công việc hoặc đời sống cá nhân, tách khỏi những ảnh hưởng cũ, đau khổ, khó khăn trước đó. Khi đạt tới đỉnh ở độ tuổi này sẽ vượt qua được (dọn nợ).",
-  2: "Khi tới đỉnh này thì đạt tới sự phát triển tâm linh, sẽ thấy mình nhạy cảm hơn, biết lắng nghe và thấu cảm hơn.",
-  3: "Học được một cái gì đó mới mẻ, nên mở mang kiến thức, đi học thêm, viết lách hoặc thuyết trình, giúp bạn thấu hiểu những kiến thức sâu sắc.",
-  4: "Những cố gắng sẽ được đền bù (trước đó cố gắng gì tới đỉnh sẽ đạt được).",
-  5: "Thay đổi nhận thức (xảy ra cái gì đó giúp ta mở mang nhận thức), thường mang bạn đi xa (du lịch, công tác nước ngoài) hoặc giúp bạn giải phóng những kìm nén trong cảm xúc. Nếu có số chủ đạo là số chẵn thì sẽ khá thành công trong tiền bạc, nếu số lẻ thành công trong việc tinh thần hay nghệ thuật.",
-  6: "Có duyên với sáng tạo (ra được các dự án sáng tạo) hoặc xây dựng tổ ấm (có thể lập gia đình/gặp bạn đời).",
+  1: "Báo hiệu một giai đoạn nỗ lực cá nhân cực kỳ mạnh mẽ mang tính thực tế cao, có những thay đổi lớn về công việc hoặc đời sống cá nhân, tách khỏi những ảnh hưởng cũ, đau khổ, khó khăn trước đó khi đạt tới đỉnh ở độ tuổi này sẽ vượt qua được (dọn nợ).",
+  2: "Khi tới đỉnh này thì đạt tới sự phát triển tâm linh, sẽ thấy mình nhạy cảm hơn, biết lắng nghe và thấu cảm hơn, đây là thời kỳ làm việc chăm chỉ nhưng tiến bộ về mặt vật chất diễn ra chậm chạp.",
+  3: "Học được một cái gì đó mới mẻ, nên mở mang kiến thức, đi học thêm, viết lách hoặc thuyết trình, giúp bạn thấu hiểu những kiến thức sâu sắc, thôi thúc đi du lịch để mở rộng tầm nhìn rất mạnh mẽ.",
+  4: "Gia tăng sức mạnh vật chất, những cố gắng sẽ được đền bù (trước đó cố gắng gì tới đỉnh sẽ đạt được).",
+  5: "Thay đổi nhận thức (xảy ra cái gì đó giúp ta mở mang nhận thức), thường mang bạn đi xa (du lịch, công tác nước ngoài) hoặc giúp bạn giải phóng những kìm nén trong cảm xúc, nổi lo âu về tài chính (nhận thức được về giá trị vật chất không còn đặt nặng ham muốn).",
+  6: "Có duyên với sáng tạo (ra được các dự án sáng tạo) hoặc tập trung mạnh mẽ vào gia đình, kết hôn (có thể quen vợ vào giai đoạn này) hoặc chăm sóc tổ ấm.",
   7: "Nhìn về mặt đời đau thương (tổn thất xảy ra và phải học để rút kinh nghiệm), bạn đã tích lũy đủ trải nghiệm để đi truyền dạy, hướng dẫn hoặc giúp đỡ người khác.",
-  8: "Có duyên hoạt động độc lập, trí tuệ (cần phải đi một mình để vượt qua), gặt hái những thành công rất lớn về mặt sự nghiệp và kinh doanh.",
-  9: "Hãy làm việc tốt cho cộng đồng, thực hiện những dự án giúp ích cho nhiều người, thể hiện lòng trắc ẩn và trách nhiệm xã hội cao cả (đỉnh này không kinh doanh được).",
+  8: "Có duyên hoạt động độc lập, trí tuệ (cần phải đi 1 mình để vượt qua), gặt hái những thành công rất lớn về mặt sự nghiệp và kinh doanh. Đối với những người có số chủ đạo chẵn, đó thường là thành công độc lập về tài chính. Đối với số chủ đạo lẻ, đó là thành công độc lập trong nghệ thuật hoặc học thuật.",
+  9: "Hãy làm việc tốt cho cộng đồng, thực hiện những dự án giúp ích cho nhiều người, thể hiện lòng trắc ẩn và trách nhiệm xã hội cao cả (đỉnh này không kinh doanh được). Thường đi kèm với sự thay đổi lớn về nghề nghiệp, nơi ở, đi du lịch hoặc tái cấu trúc các mối quan hệ lâu năm.",
   10: "Để đi giúp người khác, thực hiện những thay đổi mang tính bước ngoặt, dẫn dắt người khác hoặc tạo ra những di sản có giá trị lâu dài.",
   11: "Giúp người về tâm linh, mang lại sự chữa lành hoặc thức tỉnh cho những người xung quanh.",
 };
